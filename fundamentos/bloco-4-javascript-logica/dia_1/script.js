52d0e86f0ab012d8938d9060b9339aa4ee2f0f76
deyvid-dev <deyvidscm@hotmail.com>
2021-05-20 10:55:44 -0300

    exercicios do 5-10

--- fundamentos/bloco-4-javascript-logica/dia_1/script.js
+++ fundamentos/bloco-4-javascript-logica/dia_1/script.js
@@ -69,3 +69,115 @@ function valorDefi(a){
 
 /* 5 - Faça um programa que defina três variáveis com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false , caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.*/
 
+let angulo1 = 30;
+let angulo2 = 50;
+let angulo3 = 100;
+let triangulo = angulo1 > 0 && angulo2 > 0 && angulo3 > 0;
+let somaTri = angulo1 + angulo2 + angulo3;
+
+if(triangulo){
+  if(somaTri === 180){
+//    console.log(true);
+  } else {
+ //   console.log(false);
+  }
+} else {
+ // console.log("Erro")
+}
+
+/* 6 - Escreva um programa que receba o nome de uma peça de xadrez e retorne os movimentos que ela faz.
+Como desafio, faça o programa funcionar tanto se receber o nome de uma peça com letras maiúsculas quanto com letras minúsculas, sem aumentar a quantidade de condicionais.
+Como dica, você pode pesquisar uma função que faz uma string ficar com todas as letras minúsculas (lower case) .
+Se a peça passada for inválida, o programa deve retornar uma mensagem de erro.*/
+
+let peca;
+
+function moviPeca(peca) {
+  peca = peca.toLowerCase();
+  if(peca === "horse"){
+    console.log("Movimenta em proporção de 2:1, formando um L");
+  }
+  else if(peca === "bishop"){
+    console.log("Movimenta em diagonal");
+  }
+  else if(peca === "king"){
+    console.log("Movimenta em todas as direções somente uma casa.");
+  }else {
+    console.log("Erro.")
+  }
+}
+// moviPeca("KinG");
+
+/* 7 - Escreva um programa que converte uma nota dada em porcentagem (de 0 a 100) em conceitos de A a F. Siga essas regras:
+Porcentagem >= 90 -> A
+Porcentagem >= 80 -> B
+Porcentagem >= 70 -> C
+Porcentagem >= 60 -> D
+Porcentagem >= 50 -> E
+Porcentagem < 50 -> F
+O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100. */
+
+let nota;
+
+function converte(nota){
+  if(nota >= 90){
+    console.log("A");
+  }else if(nota >= 80){
+    console.log("B");
+  }else if(nota >= 70){
+    console.log("C");
+  }else if(nota >= 60){
+    console.log("D");
+  }else if(nota >= 50){
+    console.log("E");
+  }else if(nota < 50 && nota >= 0){
+    console.log("F");
+  }else {
+    console.log("Erro");
+  }
+}
+// converte(nota);
+
+/* 8 - Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for par. Caso contrário, ele retorna false .
+Bonus: use somente um if .*/
+
+let num1;
+let num2;
+let num3;
+
+function par(num1,num2,num3){
+  if(num1 % 2 === 0){
+    console.log(true);
+  }else if(num2 % 2 === 0){
+    console.log(true);
+  }else if(num3 % 2 === 0){
+    console.log(true);
+  }else {
+    console.log(false);
+  }
+}
+// par(9,5,4);
+
+/* 9 - Escreva um programa que defina três números em variáveis e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false .
+Bonus: use somente um if . */
+
+function impar(num1,num2,num3){
+  if(num1 % 2 !== 0){
+    console.log(true);
+  }else if(num2 % 2 !== 0){
+    console.log(true);
+  }else if(num3 % 2 !== 0){
+    console.log(true);
+  }else {
+    console.log(false);
+  }
+}
+// impar(8,5,4);
+
+/* 10 - Escreva um programa que se inicie com dois valores em duas variáveis diferentes: o custo de um produto e seu valor de venda. A partir dos valores, calcule quanto de lucro (valor de venda descontado o custo do produto) a empresa terá ao vender mil desses produtos.
+Atente que, sobre o custo do produto, incide um imposto de 20%.
+Seu programa também deve emitir uma mensagem de erro e encerrar caso algum dos seus valores de entrada seja menor que zero.
+O lucro de um produto é o resultado da subtração do valor de venda pelo custo do mesmo, sendo que o imposto de 20% também faz parte do valor de custo.
+valorCustoTotal = valorCusto + impostoSobreOCusto
+lucro = valorVenda - valorCustoTotal (lucro de um produto) */
+
