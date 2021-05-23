306633b4c286468277dd0ae3fab22237a54c1366
deyvid-dev <deyvidscm@hotmail.com>
2021-05-23 00:59:42 -0300

    conclusao dos exercicios dia 1

--- fundamentos/bloco-4-javascript-logica/dia_1/script.js
+++ fundamentos/bloco-4-javascript-logica/dia_1/script.js
@@ -195,3 +195,47 @@ if(custoProd >= 0 && valorVenda >= 0){
  // console.log("Erro, valorer menores que zero");
 }
 
+/* 11 - Uma pessoa que trabalha de carteira assinada no Brasil tem descontados de seu salário bruto o INSS e o IR. Faça um programa que, dado um salário bruto, calcule o líquido a ser recebido.
+A notação para um salário de R$1500,10, por exemplo, deve ser 1500.10. Para as faixas de impostos, use as seguintes referências:
+INSS (Instituto Nacional do Seguro Social)
+Salário bruto até R$ 1.556,94: alíquota de 8%
+Salário bruto de R$ 1.556,95 a R$ 2.594,92: alíquota de 9%
+Salário bruto de R$ 2.594,93 a R$ 5.189,82: alíquota de 11%
+Salário bruto acima de R$ 5.189,82: alíquota máxima de R$ 570,88
+IR (Imposto de Renda)
+Até R$ 1.903,98: isento de imposto de renda
+De R$ 1.903,99 a 2.826,65: alíquota de 7,5% e parcela de R$ 142,80 a deduzir do imposto
+De R$ 2.826,66 a R$ 3.751,05: alíquota de 15% e parcela de R$ 354,80 a deduzir do imposto
+De R$ 3.751,06 a R$ 4.664,68: alíquota de 22,5% e parcela de R$ 636,13 a deduzir do imposto
+Acima de R$ 4.664,68: alíquota de 27,5% e parcela de R$ 869,36 a deduzir do imposto.*/
+
+let inss;
+let ir;
+let salarioBruto = 3500;
+let salarioLiquido;
+
+if(salarioBruto <= 1556.94){
+  inss = salarioBruto * 0.08;
+} else if(salarioBruto <= 2826.65){
+  inss = salarioBruto * 0.09;
+} else if(salarioBruto <= 5189.82){
+  inss = salarioBruto * 0.11;
+} else {
+  inss = 570.80;
+}
+
+salarioLiquido = salarioBruto - inss;
+
+if(salarioLiquido <= 1903.98){
+  ir = 0;
+} else if(salarioLiquido <= 2826.65){
+  ir = (salarioLiquido * 0.075) - 142.80;
+} else if(salarioLiquido <= 3751.05){
+  ir = (salarioLiquido * 0.15) - 354.80;
+} else if(salarioLiquido <= 4664.68){
+  ir = (salarioLiquido * 0.225) - 636.13;
+} else {
+  ir = (salarioLiquido * 0.275) -869.36;
+}
+
+// console.log("Resultado do salário é: " + (salarioLiquido - ir));
